4811f11df8d38bb80e9db3e60f870de524eb4964
auto <somewater_a1@hotmail.com>
2021-08-27 19:17:45 +0700

    Upload new chart

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -1,13 +1,33 @@
 <template>
-  <div>
-    <h1>Hello graph data</h1>
+  <div id="app">
+    <p>
+      <canvas id="graph" width="400" height="200"></canvas>
+    </p>
   </div>
 </template>
 
 <script>
+import Chart from "chart.js/dist/chart";
+
 export default {
-  name: "Graph",
+  mounted() {
+    var ctx = document.getElementById("graph").getContext("2d");
+    var bar = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: ["1", "2", "3"],
+        datasets: [
+          {
+            label: "# of Vote",
+            data: [12, 45, 67],
+          },
+        ],
+      },
+    });
+    console.log(bar);
+  },
+  data() {
+    return {};
+  },
 };
 </script>
-
-<style scoped></style>
